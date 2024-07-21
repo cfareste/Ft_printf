32f689acae113e0910f3e3c576c2cafd72447b64
ChristianFidalgoAreste <chrisfidare@yahoo.com>
2024-07-22 00:36:59 +0200

    Fix printf

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -17,11 +17,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*str;
 	size_t	s1_len;
 	size_t	s2_len;
-	size_t	i;
 
 	if (!s1 || !s2)
 		return (0);
-	i = 0;
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	str = malloc(sizeof(char) * (s1_len + s2_len + 1));
--- src_bonus/ft_printf_bonus.c
+++ src_bonus/ft_printf_bonus.c
@@ -12,23 +12,23 @@
 
 #include "ft_printf_bonus.h"
 
-int	handle_parameter(char const *str, int *pos, va_list args, t_flags *flags)
+int	handle_parameter(char const *str, int *pos, va_list *args, t_flags *flags)
 {
 	char	specifier;
 
 	specifier = str[*pos];
 	if (specifier == 'c')
-		return (print_character(va_arg(args, int), flags));
+		return (print_character(va_arg(*args, int), flags));
 	else if (specifier == 's')
-		return (print_string(va_arg(args, char *), flags));
+		return (print_string(va_arg(*args, char *), flags));
 	else if (specifier == 'p')
-		return (print_pointer(va_arg(args, void *), flags));
+		return (print_pointer(va_arg(*args, void *), flags));
 	else if (specifier == 'd' || specifier == 'i')
-		return (print_number(va_arg(args, int), flags));
+		return (print_number(va_arg(*args, int), flags));
 	else if (specifier == 'u')
-		return (print_unsigned_number(va_arg(args, unsigned int), flags));
+		return (print_unsigned_number(va_arg(*args, unsigned int), flags));
 	else if (specifier == 'x' || specifier == 'X')
-		return (print_hex_nbr(va_arg(args, unsigned int), specifier, flags));
+		return (print_hex_nbr(va_arg(*args, unsigned int), specifier, flags));
 	else if (specifier == '%')
 		return (print_character('%', flags));
 	else
@@ -49,7 +49,7 @@ int	ft_printf(char const *str, ...)
 	int		i;
 	int		final_length;
 	int		bytes_written;
-	va_list	args;
+	va_list	*args;
 	t_flags	flags;
 
 	i = 0;
@@ -59,7 +59,7 @@ int	ft_printf(char const *str, ...)
 	while (str[i])
 	{
 		if (str[i] == '%')
-			bytes_written = handle_flag(str, &i, args, &flags);
+			bytes_written = handle_flag(str, &i, &args, &flags);
 		else
 			bytes_written = print_raw_char(str[i]);
 		if (bytes_written == -1)
